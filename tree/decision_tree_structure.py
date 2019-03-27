6c9f7b218ad363318d5dd77dd573244604e6bf1e
Tudor Lapusan <tudor.lapusan@gmail.com>
2019-03-27 21:05:13 +0200

    Add more visualisations

--- tree/decision_tree_structure.py
+++ tree/decision_tree_structure.py
@@ -1,9 +1,12 @@
+import sys
+
 import graphviz
 import numpy as np
 import pygraphviz as pgv
-
 from matplotlib import pyplot as plt
 
+
+# TODO calculate max_depth (is useful when max_depth is not a parameter and we use for ex. min_samples_split = 100
 
 class DecisionTreeStructure:
     def __init__(self, tree, features):
@@ -17,6 +20,7 @@ class DecisionTreeStructure:
         self.threshold = tree.tree_.threshold
         self.impurity = tree.tree_.impurity
         self.n_node_samples = tree.tree_.n_node_samples
+        self.weighted_n_node_samples = tree.tree_.weighted_n_node_samples
         self.value = tree.tree_.value
 
         self.is_leaf = []
@@ -39,7 +43,7 @@ class DecisionTreeStructure:
 
             # TODO round(self.value[node_id][0][0], 2) for regression tree
             g_tree.add_node(node_id, color="blue",
-                            label=f"Node {node_id} \n {self.features[self.feature[node_id]]} {threshold_sign} {self.threshold[node_id]} \n samples {self.n_node_samples[node_id]} \n value {self.value[node_id][0]}, \n impurity {round(self.impurity[node_id], 2)}",
+                            label=f"Node {node_id} \n {self.features[self.feature[node_id]]} {threshold_sign} {self.threshold[node_id]} \n samples {self.n_node_samples[node_id]} \n weighted sample {round(self.weighted_n_node_samples[node_id], 1)} \n value {self.value[node_id][0]}, \n impurity {round(self.impurity[node_id], 2)}",
                             fontsize=10, center=True, shape="ellipse")
 
             if self.children_left[node_id] != -1:
@@ -61,36 +65,84 @@ class DecisionTreeStructure:
             else:
                 self.is_leaf[node_id] = True
 
-    def show_leaf_impurity_distribution(self, bins=10):
+    def show_leaf_impurity_distribution(self, bins=10, figsize=None):
         if len(self.is_leaf) == 0:
             self._calculate_leaf_nodes()
 
+        if figsize:
+            plt.figure(figsize=figsize)
         plt.xticks(np.arange(0.0, 1.0, 0.05))
         plt.hist([self.impurity[i] for i in range(0, self.node_count) if self.is_leaf[i]], bins=bins)
 
-    def show_leaf_impurity(self):
+    def show_leaf_impurity(self, figsize=None):
         if len(self.is_leaf) == 0:
             self._calculate_leaf_nodes()
 
         leaf_impurity = [(i, self.impurity[i]) for i in range(0, self.node_count) if self.is_leaf[i]]
         x, y = zip(*leaf_impurity)
+
+        if figsize:
+            plt.figure(figsize=figsize)
         plt.xticks(range(0, len(x)), x)
         plt.bar(range(0, len(x)), y, label="leaf impurity")
         plt.legend()
 
-
-    def show_leaf_samples_distribution(self, bins=10):
+    def show_leaf_samples_distribution(self, bins=10, figsize=None, max_leaf_sample=sys.maxsize):
         if len(self.is_leaf) == 0:
             self._calculate_leaf_nodes()
 
-        plt.hist([self.n_node_samples[i] for i in range(0, self.node_count) if self.is_leaf[i]], bins=bins)
+        if figsize:
+            plt.figure(figsize=figsize)
+        plt.hist([self.n_node_samples[i] for i in range(0, self.node_count) if
+                  ((self.is_leaf[i]) & (self.n_node_samples[i] < max_leaf_sample))], bins=bins)
 
-    def show_leaf_samples(self):
+    def show_leaf_samples(self, figsize=None):
         if len(self.is_leaf) == 0:
             self._calculate_leaf_nodes()
 
         leaf_impurity = [(i, self.n_node_samples[i]) for i in range(0, self.node_count) if self.is_leaf[i]]
         x, y = zip(*leaf_impurity)
+
+        if figsize:
+            plt.figure(figsize=figsize)
         plt.xticks(range(0, len(x)), x)
         plt.bar(range(0, len(x)), y, label="leaf samples")
         plt.legend()
+
+    def show_leaf_samples_by_class(self, figsize=None, leaf_sample_size=None):
+        """
+        For now only for binary classification
+        """
+        if len(self.is_leaf) == 0:
+            self._calculate_leaf_nodes()
+
+        leaf_samples = [(i, self.value[i][0][0], self.value[i][0][1], self.impurity[i]) for i in
+                        range(0, self.node_count)
+                        if (self.is_leaf[i])]
+        index, leaf_samples_0, leaf_samples_1, impurity_sample = zip(*leaf_samples)
+
+        if leaf_sample_size is None:
+            leaf_sample_size = len(index)
+        if figsize:
+            plt.figure(figsize=figsize)
+        p0 = plt.bar(range(0, len(index[:leaf_sample_size])), leaf_samples_0[:leaf_sample_size])
+        p1 = plt.bar(range(0, len(index[:leaf_sample_size])), leaf_samples_1[:leaf_sample_size],
+                     bottom=leaf_samples_0[:leaf_sample_size])
+        plt.xticks(range(0, len(index)), index)
+        plt.legend((p0[0], p1[0]), ('class 0', 'class 1'))
+        plt.show()
+
+    def get_leaf_node_count(self):
+        if len(self.is_leaf) == 0:
+            self._calculate_leaf_nodes()
+
+        return sum(self.is_leaf)
+
+    def get_split_node_count(self):
+        if len(self.is_leaf) == 0:
+            self._calculate_leaf_nodes()
+
+        return len(self.is_leaf) - sum(self.is_leaf)
+
+    def get_node_count(self):
+        return self.node_count
